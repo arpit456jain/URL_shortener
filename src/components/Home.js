49f0bfea08633789767a39f33deadd6f2f1a1c5f
Arpit <111arpit1@gmail.com>
2023-07-15 12:57:05 +0530

    added use state for name and link

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,24 @@
 import React, { useState } from "react";
 import { Container, Form,Button } from "react-bootstrap";
-
+import axios from "axios";
 
 function Header() {
-// const [name,setName] = useState=("");
-// const [link,setLink] = useState("");
+const [name,setName] = useState("");
+const [link,setLink] = useState("");
 
-// function handleNameChange(event)
-// {
-//     setName(event.target.value);
-// }
+function handleNameChange(event)
+{
+    setName(event.target.value);
+}
+function handleLinkChange(event)
+{
+    setLink(event.target.value);
+}
 function handleSubmit(event) {
-    console.log("handle submit")
+    console.log("handle submit");
+    
+      setName("");
+      setLink("");
 }
 
   return (
@@ -20,10 +27,10 @@ function handleSubmit(event) {
         <Form>
         <h1>Enter your name and the Link you want to shorten</h1>
           <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-            <Form.Control type="text" placeholder="Enter your name" />
+            <Form.Control type="text" placeholder="Enter your name" value={name} onChange={handleNameChange} />
           </Form.Group>
           <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-            <Form.Control type="text" placeholder="Enter the link"/>
+            <Form.Control type="text" placeholder="Enter the link" value={link} onChange={handleLinkChange} />
           </Form.Group>
           <Button variant="info" onClick={handleSubmit}>Get Short Link</Button>
         </Form>
